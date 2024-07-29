2289981932128c381088fe7cb30da9e6eddb5a77
TanDev <dotrongtan113@gmail.com>
2024-07-29 08:53:32 +0700

    update

--- src/views/admin/MasterData/PurchaseOrder/orderList.vue
+++ src/views/admin/MasterData/PurchaseOrder/orderList.vue
@@ -1,3 +1,135 @@
+<script setup>
+import { ref } from "vue";
+const products = ref([
+  {
+    id: "001",
+    docName: "Yêu cầu đặt hàng 001",
+    docCode: "YCDHxxx",
+    createdBy: "Đại lý Trường an",
+    startDate: "21312",
+    tmpForm: "Phê duyệt 3 cấp",
+    status: "Chờ phê duyệt",
+    note: "Đơn đặt hàng 04/2024 - Đợt 2",
+  },
+  {
+    id: "002",
+    docName: "Yêu cầu đặt hàng 002",
+    docCode: "YCDHxxx",
+    createdBy: "Đại lý Trường an",
+    startDate: "21312",
+    tmpForm: "Phê duyệt 3 cấp",
+    status: "Chờ phê duyệt",
+    note: "Đơn đặt hàng 04/2024 - Đợt 2",
+  },
+  {
+    id: "003",
+    docName: "Yêu cầu đặt hàng 003",
+    docCode: "YCDHxxx",
+    createdBy: "Đại lý Trường an",
+    startDate: "21312",
+    tmpForm: "Phê duyệt 3 cấp",
+    status: "Chờ phê duyệt",
+    note: "Đơn đặt hàng 04/2024 - Đợt 2",
+  },
+  {
+    id: "003",
+    docName: "Yêu cầu đặt hàng 003",
+    docCode: "YCDHxxx",
+    createdBy: "Đại lý Trường an",
+    startDate: "21312",
+    tmpForm: "Phê duyệt 3 cấp",
+    status: "Chờ phê duyệt",
+    note: "Đơn đặt hàng 04/2024 - Đợt 2",
+  },
+  {
+    id: "003",
+    docName: "Yêu cầu đặt hàng 003",
+    docCode: "YCDHxxx",
+    createdBy: "Đại lý Trường an",
+    startDate: "21312",
+    tmpForm: "Phê duyệt 3 cấp",
+    status: "Chờ phê duyệt",
+    note: "Đơn đặt hàng 04/2024 - Đợt 2",
+  },
+  {
+    id: "003",
+    docName: "Yêu cầu đặt hàng 003",
+    docCode: "YCDHxxx",
+    createdBy: "Đại lý Trường an",
+    startDate: "21312",
+    tmpForm: "Phê duyệt 3 cấp",
+    status: "Chờ phê duyệt",
+    note: "Đơn đặt hàng 04/2024 - Đợt 2",
+  },
+]);
+
+const data = ref([
+  {
+    process: "Phê duyệt cấp 1",
+    createdBy: "hoanggggf",
+    result: "Phê duyệt",
+    approvalDate: "16/04-2024",
+  },
+  {
+    process: "Phê duyệt cấp 1",
+    createdBy: "hoanggggf",
+    result: "Phê duyệt",
+    approvalDate: "16/04-2024",
+  },
+  {
+    process: "Phê duyệt cấp 1",
+    createdBy: "hoanggggf",
+    result: "Phê duyệt",
+    approvalDate: "16/04-2024",
+  },
+]);
+</script>
 <template>
-    <div>Order list</div>
+  <div class="">
+    <div class="flex justify-content-between align-items-center mb-4">
+      <div class="text-2xl font-semibold uppercase">Báo cáo trạng thái phê duyệt</div>
+      <Button
+        label="Cập nhật"
+        icon="fa-solid fa-rotate-right"
+        class="bg-green-700"
+      ></Button>
+    </div>
+
+    <DataTable :value="products" tableStyle="min-width: 50rem;" header="surface-200">
+      <Column field="id" header="#" :style="{ width: '3%' }"></Column>
+      <Column field="docCode" header="Mã chứng từ" :style="{ width: '10%' }">
+        <template #body="data">
+          <span
+            ><i class="pi pi-arrow-right mr-3 text-yellow-500"></i
+            >{{ data.data.docCode }}</span
+          >
+        </template>
+      </Column>
+      <Column field="docName" header="Tên chứng từ" :style="{ width: '15%' }"></Column>
+      <Column field="createdBy" header="Người tạo" :style="{ width: '10%' }"></Column>
+      <Column field="status" header="Trạng thái" :style="{ width: '13%' }"></Column>
+      <Column field="note" header="Ghi chú" :style="{ width: '20%' }"></Column>
+      <Column header="Hành động" :style="{ width: '20%' }">
+        <template #body> <Button label="Phê duyệt" text outlined /> </template
+      ></Column>
+    </DataTable>
+    <Paginator class="" :rows="10" :totalRecords="120"></Paginator>
+  </div>
+
+  <!-- <div class="mt-3">
+    <DataTable :value="data" tableStyle="min-width: 50rem;" header="surface-200">
+      <Column field="process" header="Quy trình"></Column>
+      <Column field="createdBy" header="Người duyệt"></Column>
+      <Column field="result" header="Kết quả"></Column>
+      <Column field="approvalDate" header="Người duyệt"></Column>
+    </DataTable>
+  </div> -->
 </template>
+
+<style scoped>
+:deep(.p-datatable-table .p-datatable-thead tr th) {
+  background-color: #dedede;
+  color: black;
+  font-weight: bold;
+}
+</style>
--- src/views/admin/MasterData/SupplyItems/ItemGroup.vue
+++ src/views/admin/MasterData/SupplyItems/ItemGroup.vue
@@ -1,65 +1,89 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 const products = ref([
-    {
-        id: '001',
-        goodsGrpCode: 'Product 1',
-        goodsGrpName: 'goodsGrp 1',
-        desc: 10
-    },
-    {
-        id: '002',
-        goodsGrpCode: 'Product 2',
-        goodsGrpName: 'goodsGrp 2',
-        desc: 20
-    },
-    {
-        id: '003',
-        goodsGrpCode: 'Product 3',
-        goodsGrpName: 'goodsGrp 3',
-        desc: 15
-    }
+  {
+    id: "001",
+    goodsGrpCode: "HK-09",
+    goodsGrpName: "Dầu động cơ 2 kỳ",
+    desc: "",
+  },
+  {
+    id: "002",
+    goodsGrpCode: "Hk-899",
+    goodsGrpName: " Dầu nhớt xe tay ga",
+    desc: "",
+  },
+  {
+    id: "003",
+    goodsGrpCode: "HK-0944",
+    goodsGrpName: "Dầu nhớt xe số ",
+    desc: "",
+  },
 ]);
 
 const headerStyle = {
-    backgroundColor: '#DEDEDE', // choose your desired color
-    color: 'black', // text color
-    fontWeight: 'bold' // font weight if needed
-    // add more styles as needed
+  backgroundColor: "#DEDEDE", // choose your desired color
+  color: "black", // text color
+  fontWeight: "bold", // font weight if needed
+  // add more styles as needed
 };
 </script>
 
 <template>
-    <div class="flex justify-content-between align-items-center mb-4">
-        <div class="text-2xl font-semibold uppercase">Danh mục nhóm hàng hóa</div>
-        <Button label="Cập nhật" icon="fa-solid fa-rotate-right" class="bg-green-700"></Button>
-    </div>
+  <div class="flex justify-content-between align-items-center mb-4">
+    <div class="text-2xl font-semibold uppercase">Danh mục nhóm hàng hóa</div>
+    <Button
+      label="Cập nhật"
+      icon="fa-solid fa-rotate-right"
+      class="bg-green-700"
+    ></Button>
+  </div>
 
-    <div class="grid mt-3">
-        <div class="col-12 h-screen bg-white">
-            <DataTable :value="products" tableStyle="min-width: 50rem;" header="surface-200">
-                <Column field="id" header="#" :headerStyle="headerStyle" :style="{ width: '5%' }"></Column>
-                <Column field="goodsGrpCode" header="Mã nhóm hàng hóa" :headerStyle="headerStyle" :style="{ width: '20%' }"></Column>
-                <Column field="goodsGrpName" header="Tên nhóm hàng hóa" :headerStyle="headerStyle" :style="{ width: '40%' }"></Column>
-                <Column field="desc" header="Mô tả" :headerStyle="headerStyle" :style="{ width: '30%' }"></Column>
-            </DataTable>
-        </div>
+  <div class="grid mt-3">
+    <div class="col-12 h-screen bg-white">
+      <DataTable :value="products" tableStyle="min-width: 50rem;" header="surface-200">
+        <Column
+          field="id"
+          header="#"
+          :headerStyle="headerStyle"
+          :style="{ width: '5%' }"
+        ></Column>
+        <Column
+          field="goodsGrpCode"
+          header="Mã nhóm hàng hóa"
+          :headerStyle="headerStyle"
+          :style="{ width: '20%' }"
+        ></Column>
+        <Column
+          field="goodsGrpName"
+          header="Tên nhóm hàng hóa"
+          :headerStyle="headerStyle"
+          :style="{ width: '40%' }"
+        ></Column>
+        <Column
+          field="desc"
+          header="Mô tả"
+          :headerStyle="headerStyle"
+          :style="{ width: '30%' }"
+        ></Column>
+      </DataTable>
     </div>
+  </div>
 </template>
 
 <style scoped>
 :deep(.p-datatable-table .p-datatable-thead tr th) {
-    background-color: #dedede;
-    color: black;
-    font-weight: bold;
+  background-color: #dedede;
+  color: black;
+  font-weight: bold;
 }
 
 :deep(.p-datatable-thead th:nth-child(1) .p-column-header-content) {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 :deep(.p-datatable-tbody td:nth-child(1)) {
-    text-align: center;
+  text-align: center;
 }
 </style>
