cdc76a44f313b531524cdc63fb10e89b750d51fc
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-07-29 16:51:25 +0700

    u

--- src/views/client/pages/order/SearchOrder.vue
+++ src/views/client/pages/order/SearchOrder.vue
@@ -6,10 +6,9 @@
                 <span class="block text-4xl font-semibold my-4">
                     Tra cứu đơn hàng
                 </span>
-                <span class="line-height-3 text-gray-500">To trach your order please enter your order ID in the input
-                    field below and press the "Track
-                    Order" button, this was given to you on your receipt and in the confirmation email you should have
-                    received.</span>
+                <span class="line-height-3 text-gray-500">Để theo dõi đơn hàng của bạn, vui lòng nhập ID đơn hàng của
+                    bạn vào ô nhập dưới đây và nhấn nút "Theo dõi Đơn hàng". Mã ID này đã được cung cấp cho bạn trên
+                    biên nhận và trong email xác nhận bạn đã nhận được.</span>
                 <div class="grid mt-2">
                     <div class="col-6">
                         <span class="mb-2 block font-semibold">Mã đơn hàng</span>
@@ -17,12 +16,11 @@
                     </div>
                     <div class="col-6">
                         <span class="mb-2 block font-semibold">Email</span>
-                        <InputText class="w-full" placeholder="Email address" type="text" v-model="value" />
+                        <InputText class="w-full" placeholder="Địa chỉ email" type="text" v-model="value" />
                     </div>
                 </div>
-                <span class="block"><i class="fa-solid fa-circle-info"></i> Order ID that we sended to your in your
-                    email
-                    address</span>
+                <span class="block"><i class="fa-solid fa-circle-info"></i> Mã đơn hàng mà chúng tôi đã gửi đến địa chỉ
+                    email của bạn</span>
                 <router-link to="/client/order-detail"><Button style="background-color: #F2EE1B"
                         class="mt-4 w-20rem text-gray-800 border-none p-3" label="TRA CỨU ĐƠN HÀNG" /></router-link>
             </div>
