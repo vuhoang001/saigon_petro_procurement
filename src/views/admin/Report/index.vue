e3300f8199cd0c2bb9fd5ec6af566dd47d5bf08a
TanDev <dotrongtan113@gmail.com>
2024-07-29 11:05:24 +0700

    update

--- src/views/admin/Report/index.vue
+++ src/views/admin/Report/index.vue
@@ -2,9 +2,19 @@
   <div class="flex justify-content-between">
     <h5>Tổng quan</h5>
     <div class="flex gap-2">
-      <Button label="Tuỳ chỉnh" outlined />
-      <Button label="Tuỳ chỉnh" outlined />
+      <Button label="Tuỳ chỉnh" outlined icon="pi pi-cog" severity="secondary" />
+      <Button label="Tuỳ chỉnh" outlined icon="pi pi-chevron-down" severity="secondary" />
     </div>
   </div>
+  <div class="card mt-5 flex">
+    <Dropdown
+      :options="[{ name: 'Năm nay' }]"
+      optionLabel="name"
+      v-model="year"
+      class="w-full"
+    ></Dropdown>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+</script>
